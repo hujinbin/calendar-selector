a3c1a63f994423caaa17db27ba5c06d61255d654
hujinbin <437044048@qq.com>
2019-12-13 18:11:28 +0800

    edit

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12957,7 +12957,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57818" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51240" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
