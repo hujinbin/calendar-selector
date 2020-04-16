885566c5f178c72bb55d05997122a38c4aa4a445
hujinbin <437044048@qq.com>
2020-04-16 18:01:01 +0800

    edit

--- karma.conf.js
+++ karma.conf.js
@@ -27,14 +27,18 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      './test/*.js':['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ['progress','coverage'],
+    coveragereporter:{
+      type:'html',
+      dir:'coverage'
+    },
 
     // web server port
     port: 9876,
@@ -60,7 +64,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
