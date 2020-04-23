5e004a44f3455947d85e263ca89532fdc5e79818
hujinbin <437044048@qq.com>
2020-04-23 18:02:58 +0800

    karma测试用例编写

--- test/index.js
+++ test/index.js
@@ -5,10 +5,12 @@ describe('calendar.vue', () => {
   it('calendar是否存在',()=>{
     expect(calendar).to.be.ok;
   })
-  it('should render correct contents', () => {
+  it('calendar初始化是否默认选中当前时间', () => {
     const Constructor = Vue.extend(calendar)
     const vm = new Constructor().$mount()
-    // expect(vm.$el.querySelector('.hello h1').textContent)
-    //   .to.equal('Welcome to Your Vue.js App')
+    const date = new Date(); 
+    const day = date.getDate() < 10 ? "0" + String(date.getDate()) : date.getDate(); //获取当前日期；
+    expect(vm.$el.querySelector('.date .act_date span').textContent)
+      .to.equal(day)
   })
 })
